8a9e159797f07be8c6147e24b975550484093afd
umcondo <superpowerkdg@gmail.com>
2022-10-21 00:21:29 +0900

    Chore: setting RN

---
+++ App.js
@@ -0,0 +1,41 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionTitle}>안녕하쇼</Text>
+        <Text style={styles.sectionDescription}>
+          클래스 컴포넌트는 RN-0.62버전에서 봐야것소
+        </Text>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
